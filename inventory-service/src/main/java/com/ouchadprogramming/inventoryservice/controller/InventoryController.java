64b7dc5ecbfad350a28d7592055b65de4fcebfbb
ouchad97 <ouchad97@gmail.com>
2022-08-19 14:19:01 +0100

    build discovery-server

--- inventory-service/src/main/java/com/ouchadprogramming/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/ouchadprogramming/inventoryservice/controller/InventoryController.java
@@ -5,9 +5,7 @@ import com.ouchadprogramming.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -19,7 +17,6 @@ public class InventoryController {
     // http://localhost:8082/api/inventory/iphone-13,iphone13-red
 
     // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
--- order-service/src/main/java/com/ouchadprogramming/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ouchadprogramming/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -41,13 +41,12 @@ public class OrderService {
 
         // Call Inventory Service, and place order if product is in
         // stock
-        InventoryResponse[] inventoryResponsArray = webClient.get()
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
                 .uri("http://localhost:8082/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
-
 
         boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
                 .allMatch(InventoryResponse::isInStock);
