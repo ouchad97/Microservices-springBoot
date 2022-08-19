1d8d1e21ece4549a67cad1c9e1a04df89b9bac4a
ouchad97 <ouchad97@gmail.com>
2022-08-19 09:40:45 +0100

    reconfig --->MongoDB

--- product-service/src/main/java/com/ouchadprogramming/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/ouchadprogramming/productservice/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
 
+    
     private final ProductService productService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
